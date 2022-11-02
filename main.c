2a0ca0ae4ba087ea5d9ad741ec784796c710336d
ThibaultL7 <thibault.leonardon@edu.ece.fr>
2022-11-02 16:26:46 +0100

    <thibault.leonardon@edu.ece.fr> son + chrono

--- main.c
+++ main.c
@@ -8,11 +8,12 @@ int main()
     install_keyboard();
     install_mouse();
     install_sound(DIGI_AUTODETECT,MIDI_AUTODETECT,"A");
-    srand(time(NULL));
+    time_t tempsdep = time(NULL);
+
 
     //Création et déclaration des bitmaps
     BITMAP* page=create_bitmap(1024,768);
-
+    SAMPLE* musiqueFond = load_sample("musique/musique_fond.wav");
 
     //Déclaration des variables
     int choixMenu;
@@ -21,10 +22,16 @@ int main()
 
     while(!key[KEY_ESC])
     {
+        clear_bitmap(page);
+        //play_sample(musiqueFond,25,0,1000,1);
         //choixMenu = menuJeu(page);
-        //actionChoixMenu(choixMenu);
+        //actionChoixMenu(choixMenu,page);
+        time_t temps2 = time(NULL);
+        unsigned long diff=difftime(temps2,tempsdep);
         creer_map(page);
-
+        textprintf_ex(page,font,980,750,makecol(255,255,255),-1,"%d",diff);
+        show_mouse(page);
+        blit(page,screen,0,0,0,0,1024,768);
 
     }
     return 0;
--- map.c
+++ map.c
@@ -3,11 +3,22 @@
 void creer_map(BITMAP* page)
 {
     BITMAP* tuile = load_bitmap("map/tuile_test.bmp",NULL);
-    for (int i = 0; i<35; i++)
-        for(int j = 0; j<45; j++)
-        {
-            masked_stretch_blit(tuile,page,0,0,77,77,j*20,i*20,20,20);
-            blit(page,screen,0,0,0,0,1024,768);
-        }
 
+    int map[35][45];
+    for (int i = 0; i<35; i++) {
+        for (int j = 0; j < 45; j++) {
+            map[i][j] = 1;
+        }
+    }
+
+    for (int i = 0; i<35; i++) {
+        for (int j = 0; j < 45; j++) {
+            if (map[i][j] == 1) {
+                masked_stretch_blit(tuile, page, 0, 0, 77, 77, j * 20, i * 20, 20, 20);
+            }
+        }
+    }
+
+    show_mouse(page);
+    blit(page,screen,0,0,0,0,1024,768);
 }
