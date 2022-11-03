8c4e1cfba8b25e9b7d31862a83b82f1bb97f1dbb
LeaEce <lea.gauthier@edu.ece.fr>
2022-11-03 13:46:22 +0100

    test commit

---
+++ cmake-build-debug/CMakeFiles/Projet_ECECity.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Projet_ECECity.dir/MenuJeu.c.obj"
+  "CMakeFiles/Projet_ECECity.dir/MenuJeu.c.obj.d"
+  "CMakeFiles/Projet_ECECity.dir/gestionBouton.c.obj"
+  "CMakeFiles/Projet_ECECity.dir/gestionBouton.c.obj.d"
+  "CMakeFiles/Projet_ECECity.dir/init_allegro.c.obj"
+  "CMakeFiles/Projet_ECECity.dir/init_allegro.c.obj.d"
+  "CMakeFiles/Projet_ECECity.dir/interface.c.obj"
+  "CMakeFiles/Projet_ECECity.dir/interface.c.obj.d"
+  "CMakeFiles/Projet_ECECity.dir/main.c.obj"
+  "CMakeFiles/Projet_ECECity.dir/main.c.obj.d"
+  "CMakeFiles/Projet_ECECity.dir/map.c.obj"
+  "CMakeFiles/Projet_ECECity.dir/map.c.obj.d"
+  "CMakeFiles/Projet_ECECity.dir/sauvegarde.c.obj"
+  "CMakeFiles/Projet_ECECity.dir/sauvegarde.c.obj.d"
+  "Projet_ECECity.exe"
+  "Projet_ECECity.exe.manifest"
+  "Projet_ECECity.pdb"
+  "libProjet_ECECity.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Projet_ECECity.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
