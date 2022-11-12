ffc6b66da0c52a72260ea693dbf2add4a248c739
m28wa <maxime.warembourg@edu.ece.fr>
2022-11-12 17:07:25 +0100

    case libre terminé

--- case.c
+++ case.c
@@ -6,9 +6,10 @@ void affichagecasefree(BITMAP* page,int map[35][45],int type)
     show_mouse(page);
     int value;
     int matricelibre[35][45];
-    for (int i=0 ; i<=35; i++)
+
+    for (int i=0 ; i<35; i++)
     {
-        for(int j=0;j<=45; j++)
+        for(int j=0;j<45; j++)
         {
             matricelibre[i][j]=1;
             if(map[i][j]==0)
@@ -28,9 +29,9 @@ void affichagecasefree(BITMAP* page,int map[35][45],int type)
                 if(matricelibre[i][j]==0 &&
                 (map[i+1][j]==1 || map[i+1][j+1]==1 || map[i+1][j-1]==1 ||
                 map[i-1][j]==1 || map[i-1][j-1]==1 || map[i-1][j+1]==1 ||
-                map[i][j+1]==1 || map[i][j-1]==1 ) )
+                map[i][j+1]==1 || map[i][j-1]==1) )
                 {
-                    blit(tuile_surbrillance, page, 0, 0, 20, 20, j * 20, i * 20);
+                    masked_stretch_blit(tuile_surbrillance, page, 0, 0, 20, 20, j * 20, i * 20, 20, 20);
                 }
             }
         }
@@ -41,12 +42,15 @@ void affichagecasefree(BITMAP* page,int map[35][45],int type)
         {
             for(int j=0;j<45; j++)
             {
-                if(matricelibre[i][j]==0 && matricelibre[i+1][j] && matricelibre[i+2][j] &&
-                matricelibre[i][j-1] && matricelibre[i+1][j-1] && matricelibre[i+2][j-1]==0 &&
-                matricelibre[i][j-2]==0 && matricelibre[i=1][j-2]==0 && matricelibre[i+2][j-2] &&
-                ( map[i-1][j+1]==1 || map[i][j+1]==1 || map[i+1][j+1]==1 || map[i+2][j+1]==1 || map[i+3][j+1]==1 || map[i+3][j]==1 || map[i+3][j-1]==1 || map[i+3][j-2]==1 || map[i+3][j-3]==1 || map[i+2][j-3]==1 || map[i+1][j-3]==1 || map[i][j-3]==1 || map[i-1][j-3]==1 || map[i-1][j-2]==1 || map[i-1][j-1]==1 || map[i-1][j]==1) )
+                if((matricelibre[i][j]==0 && matricelibre[i][j+1]==0 && matricelibre[i][j+2]==0 &&
+                matricelibre[i-1][j]==0 && matricelibre[i-1][j+1]==0 && matricelibre[i-1][j+2]==0 &&
+                matricelibre[i-2][j]==0 && matricelibre[i-2][j+1]==0 && matricelibre[i-2][j+2]==0) &&
+                ( map[i+1][j-1]==1 || map[i+1][j]==1 || map[i+1][j+1]==1 || map[i+1][j+2]==1 ||
+                map[i+1][j+3]==1 || map[i][j+3]==1 || map[i-1][j+3]==1 || map[i-2][j+3]==1 ||
+                map[i-3][j+3]==1 || map[i-3][j+2]==1 || map[i-3][j+1]==1 || map[i-3][j]==1 ||
+                map[i-3][j-1]==1 || map[i-2][j-1]==1 || map[i-1][j-1]==1 || map[i][j]-1==1) )
                 {
-                    blit(tuile_surbrillance, page, 0, 0, 20, 20, j * 20, i * 20);
+                    masked_stretch_blit(tuile_surbrillance, page, 0, 0, 20, 20, j * 20, i * 20, 20, 20);
                 }
             }
         }
@@ -57,9 +61,18 @@ void affichagecasefree(BITMAP* page,int map[35][45],int type)
         {
             for(int j=0;j<45; j++)
             {
-                if(matricelibre[i][j]==0 && matricelibre[i+1][j]==0 && matricelibre[i+2][j]==0 && matricelibre[i+3][j]==0 && matricelibre[i][j-1]==0 && matricelibre[i+1][j-1]==0 && matricelibre[i+1][j-1]==0 && matricelibre[i+3][j-1]==0 && matricelibre[i][j-2]==0 && matricelibre[i+1][j-2]==0 && matricelibre[i+2][j-2]==0 && matricelibre[i+3][j-2]==0 && matricelibre[i][j-3]==0 &&  matricelibre[i+1][j-3]==0 && matricelibre[i+2][j-3]==0 && matricelibre[i+3][j-3]==0 && matricelibre[i][j-4]==0 && matricelibre[i+1][j-4]==0 && matricelibre[i+2][j-4]==0 && matricelibre[i+3][j-4]==0 && matricelibre[i][j-5]==0 && matricelibre[i+1][j-5]==0 && matricelibre[i+2][j-5]==0 && matricelibre[i+3][j-5]==0 && ( map[i-1][j+1]==1 || map[i][j+1]==1 || map[i+1][j+1]==1 || map[i+2][j+1]==1 || map[i+3][j+1]==1 || map[i+4][j+1]==1 || map[i+4][j]==1 || map[i+4][j-1]==1 || map[i+4][j-2]==1 || map[i+4][j-3]==1 || map[i+4][j-4]==1 || map[i+4][j-5]==1 || map[i+4][j-6]==1 || map[i+3][j-6]==1|| map[i+2][j-6]==1 || map[i+1][j-6]==1 || map[i][j-6]==1 || map[i-1][j-6]==1 || map[i-1][j-5]==1 || map[i-1][j-4]==1 || map[i-1][j-3]==1 || map[i-1][j-2]==1 || map[i-1][j-1]==1 || map[i-1][j]==1))
+                if(matricelibre[i][j]==0 && matricelibre[i][j+1]==0 && matricelibre[i][j+2]==0 && matricelibre[i][j+3]==0 &&
+                matricelibre[i-1][j]==0 && matricelibre[i-1][j+1]==0 && matricelibre[i-1][j+2]==0 && matricelibre[i-1][j+3]==0 &&
+                matricelibre[i-2][j]==0 && matricelibre[i-2][j+1]==0 && matricelibre[i-2][j+2]==0 && matricelibre[i-2][j+3]==0 &&
+                matricelibre[i-3][j]==0 && matricelibre[i-3][j+1]==0 && matricelibre[i-3][j+2]==0 && matricelibre[i-3][j+3]==0 &&
+                matricelibre[i-4][j]==0 && matricelibre[i-4][j+1]==0 && matricelibre[i-4][j+2]==0 && matricelibre[i-4][j+3]==0 &&
+                matricelibre[i-5][j]==0 && matricelibre[i-5][j+1]==0 && matricelibre[i-5][j+2]==0 && matricelibre[i-5][j+3]==0 &&
+                ( map[i+1][j-1]==1 || map[i+1][j]==1 || map[i+1][j+1]==1 || map[i+1][j+2]==1 || map[i+1][j+3]==1 ||
+                map[i+1][j+4]==1 || map[i][j+4]==1 || map[i-1][j+4]==1 || map[i-2][j+4]==1 || map[i-3][j+4]==1 || map[i-4][j+4]==1 || map[i-5][j+4]==1 ||
+                map[i-6][j+4]==1 || map[i-6][j+3]==1|| map[i-6][j+2]==1 || map[i-6][j+1]==1 || map[i-6][j]==1 ||
+                map[i-6][j-1]==1 || map[i-5][j-1]==1 || map[i-4][j-1]==1 || map[i-3][j-1]==1 || map[i-2][j-1]==1 || map[i-1][j-1]==1 || map[i][j-1]==1))
                 {
-                    blit(tuile_surbrillance, page, 0, 0, 20, 20, j * 20, i * 20);
+                    masked_stretch_blit(tuile_surbrillance, page, 0, 0, 20, 20, j * 20, i * 20, 20, 20);
                 }
             }
         }
@@ -70,9 +83,16 @@ void affichagecasefree(BITMAP* page,int map[35][45],int type)
         {
             for(int j=0;j<45; j++)
             {
-                if(matricelibre[i][j]==0 && matricelibre[i+1][j]==0 && matricelibre[i+2][j]==0 && matricelibre[i+3][j]==0 && matricelibre[i+4][j]==0 && matricelibre[i+5][j]==0 && matricelibre[i][j-1]==0 && matricelibre[i+1][j-1]==0 && matricelibre[i+2][j-1]==0 && matricelibre[i+3][j-1]==0 && matricelibre[i+4][j-1]==0 && matricelibre[i+5][j-1]==0 && matricelibre[i][j-2]==0 && matricelibre[i+1][j-2]==0 && matricelibre[i+2][j-2]==0 && matricelibre[i+3][j-2]==0 && matricelibre[i+4][j-2]==0 && matricelibre[i+5][j-2]==0 && matricelibre[i][j-3]==0 && matricelibre[i+1][j-3]==0 && matricelibre[i+2][j-3]==0 && matricelibre[i+3][j-3]==0 && matricelibre[i+4][j-3]==0 && matricelibre[i+5][j-3]==0 && ( map[i-1][j+1]==1 || map[i][j+1]==1 || map[i+1][j+1]==1 || map[i+2][j+1]==1 || map[i+3][j+1]==1 || map[i+4][j+1]==1 || map[i+5][j+1]==1 || map[i+6][j+1]==1 || map[i+6][j]==1 || map[i+6][j-11]==1 || map[i+6][j-2]==1 || map[i+6][j-3]==1 || map[i+6][j-4]==1 || map[i+5][j-4]==1 || map[i+4][j-4]==1 || map[i+3][j-4]==1 || map[i+2][j-4]==1 || map[i+1][j-4]==1 || map[i][j-4]==1 || map[i-1][j-4]==1 || map[i-1][j-3]==1 || map[i-1][j-2]==1 || map[i-1][j-1]==1 || map[i-1][j]==1))
+                if(matricelibre[i][j]==0 && matricelibre[i][j+1]==0 && matricelibre[i][j+2]==0 && matricelibre[i][j+3]==0 && matricelibre[i][j+4]==0 && matricelibre[i][j+5]==0 &&
+                matricelibre[i-1][j]==0 && matricelibre[i-1][j+1]==0 && matricelibre[i-1][j+2]==0 && matricelibre[i-1][j+3]==0 && matricelibre[i-1][j+4]==0 && matricelibre[i-1][j+5]==0 &&
+                matricelibre[i-2][j]==0 && matricelibre[i-2][j+1]==0 && matricelibre[i-2][j+2]==0 && matricelibre[i-2][j+3]==0 && matricelibre[i-2][j+4]==0 && matricelibre[i-2][j+5]==0 &&
+                matricelibre[i-3][j]==0 && matricelibre[i-3][j+1]==0 && matricelibre[i-3][j+2]==0 && matricelibre[i-3][j+3]==0 && matricelibre[i-3][j+4]==0 && matricelibre[i-3][j+5]==0 &&
+                ( map[i+1][j-1]==1 || map[i+1][j]==1 || map[i+1][j+1]==1 || map[i+1][j+2]==1 || map[i+1][j+3]==1 || map[i+1][j+4]==1 || map[i+1][j+5]==1 ||
+                map[i+1][j+6]==1 || map[i][j+6]==1 || map[i-1][j+6]==1 || map[i-2][j+6]==1 || map[i-3][j+6]==1 ||
+                map[i-4][j+6]==1 || map[i-4][j+5]==1 || map[i-4][j+4]==1 || map[i-4][j+3]==1 || map[i-4][j+2]==1 || map[i-4][j+1]==1 || map[i-4][j]==1 ||
+                map[i-4][j-1]==1 || map[i-3][j-1]==1 || map[i-2][j-1]==1 || map[i-1][j-1]==1 || map[i][j-1]==1))
                 {
-                    blit(tuile_surbrillance, page, 0, 0, 20, 20, j * 20, i * 20);
+                    masked_stretch_blit(tuile_surbrillance, page, 0, 0, 20, 20, j * 20, i * 20, 20, 20);
                 }
             }
         }
--- construction.c
+++ construction.c
@@ -11,9 +11,9 @@ void route (BITMAP* page, int map[35][45],time_t tempsdepart, int* banque, t_hab
 
     while(clic!=1)
     {
-        afficher_map(page,map);
+
         afficherInterface(page,map,tempsdepart,*banque);
-       // affichagecasefree(page,map,1);
+        affichagecasefree(page,map,1);
         test_temps(map,tab_hab,banque,nb_hab);
         if(((mouse_x>=(0)&& mouse_x<=(900))&& ((mouse_y)>=(0)&& mouse_y<=(700)))&&(mouse_b &1))
         {
@@ -24,6 +24,7 @@ void route (BITMAP* page, int map[35][45],time_t tempsdepart, int* banque, t_hab
         if(testclic==1 && *banque>10 && map[y][x]==0) {
             map[y][x] = 1;
             *banque-=10;
+            afficher_map(page,map);
         }
         if(mouse_b&2)
             clic=1;
@@ -42,13 +43,14 @@ void habitation (BITMAP* page, int map[35][45],time_t tempsdepart, int* banque, 
 
     while(clic!=1)
     {
-        if(((mouse_x>=(0)&& mouse_x<=(900))&& ((mouse_y)>=(0)&& mouse_y<=(700)))) {
+        afficherInterface(page,map,tempsdepart,*banque);
+        affichagecasefree(page,map,3);
+        /*if(((mouse_x>=(0)&& mouse_x<=(900))&& ((mouse_y)>=(0)&& mouse_y<=(700)))) {
 
-            afficher_map(page,map);
             afficherInterface(page,map,tempsdepart, *banque);
             blit(terrain_vague, page, 0, 0, mouse_x, mouse_y-40, terrain_vague->w, terrain_vague->h);
             blit(page,screen,0,0,0,0,900,700);
-        }
+        }*/
 
 
         if(((mouse_x>=(0)&& mouse_x<=(900))&& ((mouse_y)>=(0)&& mouse_y<=(700)))&&(mouse_b &1))
@@ -56,6 +58,7 @@ void habitation (BITMAP* page, int map[35][45],time_t tempsdepart, int* banque, 
             x=mouse_x/20;
             y=mouse_y/20;
             testclic=1;
+
         }
         if(testclic==1 && *banque>1000 && map[y][x]==0 && map[y][x+1]==0 && map[y][x+2]==0 && map[y-1][x]==0 && map[y-1][x+1]==0 && map[y-1][x+2]==0 && map[y-2][x]==0 && map[y-2][x+1]==0 && map[y-2][x+2]==0) {
             map[y][x]=9;
@@ -67,10 +70,8 @@ void habitation (BITMAP* page, int map[35][45],time_t tempsdepart, int* banque, 
             map[y-2][x]=3;
             map[y-2][x+1]=9;
             map[y-2][x+2]=9;
-
+            afficher_map(page,map);
             *banque-=1000;
-
-
             /* *nb_hab++;
             tab_hab=(t_habitation*)realloc(tab_hab, *nb_hab * sizeof(t_habitation));
 
@@ -83,14 +84,14 @@ void habitation (BITMAP* page, int map[35][45],time_t tempsdepart, int* banque, 
             tab_hab[*nb_hab].tempsCrea=time(NULL);
             tab_hab[*nb_hab].x=x;
             tab_hab[*nb_hab].y=y-2;
-
             *nb_hab+=1;
         }
+
+
         if(mouse_b&2)
             clic=1;
 
         test_temps(map,tab_hab,banque,*nb_hab);
-        afficher_map(page,map);
         afficherInterface(page,map,tempsdepart, *banque);
         show_mouse(page);
         blit(page,screen,0,0,0,0,900,700);
@@ -107,12 +108,13 @@ void chateau_eau(BITMAP* page, int map[35][45],time_t tempsdepart, int* banque, 
 
     while(clic!=1)
     {
-        //afficher_map(page,map);
-        if(((mouse_x>=(0)&& mouse_x<=(900))&& ((mouse_y)>=(0)&& mouse_y<=(700)))) {
+        afficherInterface(page,map,tempsdepart,*banque);
+        affichagecasefree(page,map,14);
+        /*if(((mouse_x>=(0)&& mouse_x<=(900))&& ((mouse_y)>=(0)&& mouse_y<=(700)))) {
 
             blit(chateauEauTran, page, 0, 0, mouse_x-20, mouse_y-20, chateauEauTran->w, chateauEauTran->h);
             afficherInterface(page,map,tempsdepart, *banque);
-        }
+        }*/
 
         if(((mouse_x>=(0)&& mouse_x<=(900))&& ((mouse_y)>=(0)&& mouse_y<=(700)))&&(mouse_b &1))
         {
@@ -123,10 +125,11 @@ void chateau_eau(BITMAP* page, int map[35][45],time_t tempsdepart, int* banque, 
         test_temps(map,tab_hab,banque,nb_hab);
         afficher_map(page,map);
         afficherInterface(page,map,tempsdepart, *banque);
-        if(testclic==1 && *banque>=100000 && map[y][x]==0 && (map[y][x] == 0)&&(map[y-1][x] == 0)&&(map[y-2][x] == 0)&&(map[y-3][x] == 0)&&(map[y-4][x] == 0)&&(map[y-5][x] == 0)&&(map[y][x+1]==0)
-           &&(map[y-1][x+1]==0) && (map[y-2][x+1]==0) &&(map[y-3][x+1]==0)&&(map[y-4][x+1]==0)&&(map[y-5][x+1]==0)&&(map[y][x+2]==0)&&
-           (map[y-1][x+2]==0)&&(map[y-2][x+2]==0)&&(map[y-3][x+2]==0)&&(map[y-4][x+2]==0)&&(map[y-5][x+2]==0)&&(map[y][x+3]==0)&&
-           (map[y-1][x+3]==0)&&(map[y-2][x+3]==0)&&(map[y-3][x+3]==0)&&(map[y-4][x+3]==0)&&(map[y-5][x+3]==0))
+        if(testclic==1 && *banque>=100000 &&
+            map[y][x]==0&&(map[y-1][x] == 0)&&(map[y-2][x] == 0)&&(map[y-3][x] == 0)&&(map[y-4][x] == 0)&&(map[y-5][x] == 0)&&
+           (map[y][x+1]==0)&&(map[y-1][x+1]==0)&&(map[y-2][x+1]==0)&&(map[y-3][x+1]==0)&&(map[y-4][x+1]==0)&&(map[y-5][x+1]==0)&&
+           (map[y][x+2]==0)&&(map[y-1][x+2]==0)&&(map[y-2][x+2]==0)&&(map[y-3][x+2]==0)&&(map[y-4][x+2]==0)&&(map[y-5][x+2]==0)&&
+           (map[y][x+3]==0)&&(map[y-1][x+3]==0)&&(map[y-2][x+3]==0)&&(map[y-3][x+3]==0)&&(map[y-4][x+3]==0)&&(map[y-5][x+3]==0))
         {
             map[y][x] = 16;
             map[y-1][x] =16;
@@ -156,6 +159,7 @@ void chateau_eau(BITMAP* page, int map[35][45],time_t tempsdepart, int* banque, 
             map[y-4][x+3] =16;
             map[y-5][x+3] =16;
             *banque-=100000;
+            afficher_map(page,map);
         }
         if(mouse_b&2)
             clic=1;
