a2ea1fdae147f3c3d52e0d2d97694741101f31de
ThibaultL7 <thibault.leonardon@edu.ece.fr>
2022-11-26 01:14:05 +0100

    <thibault.leonardon@edu.ece.fr> com cycles

--- gestion_cycles.c
+++ gestion_cycles.c
@@ -4,23 +4,23 @@
 
 
 #include "header.h"
-
+///Test si evolution ou regression///
 void test_temps(int map[35][45], t_habitation tab_hab[50], int* argent, int nb_hab, int nb_elec, t_centrales tab_elec[20])
 {
-    time_t tempsact = time(NULL);
+    time_t tempsact = time(NULL);//On récupere le temps actuel
     unsigned long diff;
     int x;
     int y;
-    for (int i=0; i<nb_hab; i++)
+    for (int i=0; i<nb_hab; i++)    //On parcourt toutes les habitations crées
     {
         int avant=0,apres=0;
-        diff=difftime(tempsact,tab_hab[i].tempsCrea);
-        if (diff>=15)
+        diff=difftime(tempsact,tab_hab[i].tempsCrea); //On calcule le temps depuis la création ou depuis la derniere evolution
+        if (diff>=15) //Si le temps est sup ou egal a 15
         {
             x=tab_hab[i].x;
             y=tab_hab[i].y;
-            tab_hab[i].tempsCrea=time(NULL);
-            if(tab_hab[i].elec==1)
+            tab_hab[i].tempsCrea=time(NULL);  //On remet le temps actuel comme nouveau temps pour l'habitation
+            if(tab_hab[i].elec==1) //Si la maison est alimentée, on augmente de niveau
             {
                 if(tab_hab[i].niveau<4)
                 {
@@ -35,8 +35,8 @@ void test_temps(int map[35][45], t_habitation tab_hab[50], int* argent, int nb_h
                     tab_hab[i].nb_habitants=100;
                 if(tab_hab[i].niveau==4)
                     tab_hab[i].nb_habitants=1000;
-                apres=distribution(nb_elec,nb_hab,tab_elec,tab_hab);
-                if (tab_hab[i].elec!=1)
+                apres=distribution(nb_elec,nb_hab,tab_elec,tab_hab); //On lance une distribution electrique pour réactualiser les valeurs
+                if (tab_hab[i].elec!=1) //Si apres amelioration, la maison n'est plus alimentée on la remet comme elle était, car cela signifie qu'on ne dispose pas d'assez de ressources pour evoluer
                 {
                     if(tab_hab[i].niveau>0)
                     {
@@ -55,7 +55,7 @@ void test_temps(int map[35][45], t_habitation tab_hab[50], int* argent, int nb_h
                         tab_hab[i].nb_habitants=1000;
                 }
             }
-            if (tab_hab[i].elec==0)
+            if (tab_hab[i].elec==0)//Si la maison n'est pas alimentée,on régresse
             {
                 if(tab_hab[i].niveau>0)
                 {
@@ -73,8 +73,8 @@ void test_temps(int map[35][45], t_habitation tab_hab[50], int* argent, int nb_h
                 if(tab_hab[i].niveau==4)
                     tab_hab[i].nb_habitants=1000;
             }
-            distribution(nb_elec,nb_hab,tab_elec,tab_hab);
-            *argent+= impots*tab_hab[i].nb_habitants;
+            distribution(nb_elec,nb_hab,tab_elec,tab_hab);// On réactualise la distribution
+            *argent+= impots*tab_hab[i].nb_habitants;// on paie les impots
         }
     }
 }
